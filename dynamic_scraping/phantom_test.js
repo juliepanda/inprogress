bb09938b535bd873c337a15a93a6a16c65d8ee63
Julie Pan <juliepan@cisco.com>
2015-06-16 10:02:25 -0700

    submission credentials to enter next page

--- dynamic_scraping/phantom_test.js
+++ dynamic_scraping/phantom_test.js
@@ -1,7 +1,6 @@
 var url = 'https://www.dmv.ca.gov/foa/clear.do?goTo=driveTest',
   jquery = 'http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
 
-
 var Q = require('q');
 
 var openPage = function (page, url) {
@@ -16,12 +15,13 @@ var includeJS = function (page) {
   return deferred.promise;
 };
 
-var evaluatePage = function (page) {
+var fillFields = function (page) {
   var deferred = Q.defer();
   page.evaluate( function() {
     /*
      * declare VARIABLES here, don't ask my why I couldn't do it outside..i'll fig it out later
      * */
+
     document.querySelector("select#officeId").value = SANTA_CLARA;
     document.querySelector("input[name='firstName']").value = FIRST_NAME;
     document.querySelector("input[name='lastName']").value = LAST_NAME;
@@ -37,32 +37,19 @@ var evaluatePage = function (page) {
   return deferred.promise;
 };
 
+var clickSubmit = function (page) {
+  var deferred = Q.defer();
+  page.evaluate( function () {
+    document.querySelector("input[value='Submit']").click();
+    console.log('cool');
+  });
+};
+
 var screenCapture = function (page) {
   var deferred = Q.defer();
   page.render('image.png');
   return deferred.promise;
 };
-
-/*openPage(page, url).then(function() {
-  includeJs(page);
-  evaluatePage(function() {
-  document.querySelector("select#officeId").value = SANTA_CLARA;
-  document.querySelector("input[name='firstName']").value = FIRST_NAME;
-  document.querySelector("input[name='lastName']").value = LAST_NAME;
-  document.querySelector("input[name='dlNumber']").value = DL_NUMBER;
-  document.querySelector("input[name='birthMonth']").value = B_MONTH;
-  document.querySelector("input[name='birthDay']").value = B_DAY;
-  document.querySelector("input[name='birthYear']").value = B_YEAR;
-  document.querySelector("input[name='telArea']").value = TEL_AREA;
-  document.querySelector("input[name='telPrefix']").value = TEL_PREFIX;
-  document.querySelector("input[name='telSuffix']").value = TEL_SUFFIX;
-  document.querySelector("input[name='requestedTask'][value='DT']:checked");
-  $("button[type=submit]").click();
-  });
-  screenCapture(page);
-  phantom.exit();
-  });
-  */
 
 var page = require('webpage').create();
 
@@ -71,14 +58,20 @@ openPage(page, url)
   includeJS(page);
   console.log('BIG POO'); // step 1
 })
-.then(function() {
+.then( function() {
   console.log('middling poo'); // step 2
-  evaluatePage(page);
+  fillFields(page);
 })
 .then( function() {
-  console.log('poo'); // step 3
-  screenCapture(page);
-  phantom.exit();
+  console.log('another middling poo'); // step 3
+  clickSubmit(page);
+})
+.then( function() {
+  console.log('poo'); // step 4
+  setTimeout(function() {
+    screenCapture(page);
+    phantom.exit();
+  }, 5000);
 });
 
 
